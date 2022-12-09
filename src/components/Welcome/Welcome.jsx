d245ebef4e2646e9b4e934c37607070856e7ed0b
Zerogi <rob.zgonnikov@gmail.com>
2022-12-09 16:15:16 +0300

    - continue Welcome.jsx

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import {Row, Col} from "react-bootstrap";
 
+import WelcomeDoctor from '../../assets/images/welcome-doctor.png'
+import styles from './Welcome.module.css'
+
 const Welcome = () => {
     return (
-        <div>
+        <div className={styles.welcome_wrapper}>
             <Container>
                 <Row>
                     <Col md='7'>
-                        1
+                        <h1>Красивая улыбка меняет Всё!</h1>
                     </Col>
                     <Col md='5'>
-                        2
+                        <img src={WelcomeDoctor}/>
                     </Col>
                 </Row>
             </Container>
